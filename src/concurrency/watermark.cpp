2a57d726fe17acd142d885e4e5f0191390c9d84d
Peter Moroz <ptr.moroz@gmail.com>
2024-05-22 16:52:33 +0300

    add: project4 - Task #1 (Timestamps)

--- src/concurrency/watermark.cpp
+++ src/concurrency/watermark.cpp
@@ -10,10 +10,17 @@ auto Watermark::AddTxn(timestamp_t read_ts) -> void {
   }
 
   // TODO(fall2023): implement me!
+  current_reads_[read_ts]++;
+  watermark_ = current_reads_.cbegin()->first;
 }
 
 auto Watermark::RemoveTxn(timestamp_t read_ts) -> void {
   // TODO(fall2023): implement me!
+  BUSTUB_ASSERT(current_reads_[read_ts] > 0, "unexistent timestamp");
+  if (--current_reads_[read_ts] == 0) {
+    current_reads_.erase(read_ts);
+    watermark_ = current_reads_.empty() ? 0 : current_reads_.cbegin()->first;
+  }
 }
 
 }  // namespace bustub
